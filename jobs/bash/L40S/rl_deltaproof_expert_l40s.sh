da91e3cfc784d2d058f10d7ef9f2b56bd94bf977
magn3144 <magnus@elgaard.com>
2026-09-18 09:55:00 +0200

    Created L40S STP RL experiments that run on one L40S GPU

---
+++ jobs/bash/L40S/rl_deltaproof_expert_l40s.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#BSUB -J deltaproof-expert
+#BSUB -q gpul40s
+#BSUB -W 24:00
+#BSUB -n 48
+#BSUB -R "span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -M 10GB
+#BSUB -oo jobs/logs/rl_deltaproof_expert_%J.out
+#BSUB -eo jobs/logs/rl_deltaproof_expert_%J.err
+
+source "$LS_SUBCWD/jobs/bash/common.sh"
+module load cuda/12.6.3
+export DELTA_PROOF="${DELTA_PROOF:-$LS_SUBCWD/../delta-proof}"
+
+start_round="${START_ROUND:-0}"
+
+python -u RL/run_rl_steps.py \
+    --config jobs/yaml/L40S/rl_deltaproof_expert_L40S.yaml \
+    --start-round "$start_round"
